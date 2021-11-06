fb1ba43fa61232602543bb3d6fba983b2d262ccc
Luke Percy <lpercyagile@gmail.com>
2021-11-07 11:28:28 +1300

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       <Component {...pageProps} />
