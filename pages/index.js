211f111327530905b6304c90afa72c99a2c5fc28
Luke Percy <lpercyagile@gmail.com>
2021-11-24 20:37:01 +1300

    moved github api to folder

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import ContainerBlock from "../components/ContainerBlock";
 import FavouriteProjects from "../components/FavouriteProjects";
 import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
-import getLatestRepos from "../lib/getLatestRepos";
+import getLatestRepos from "../lib/githubapi/getLatestRepos";
 import userData from "../constants/data";
 
 export default function Home({ repositories }) {
