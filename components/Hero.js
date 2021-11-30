e132673c9135cbe3c6d11c1035f04ceb440ae939
Luke Percy <lpercyagile@gmail.com>
2021-11-30 22:19:13 +1300

    centered hero text a bit

--- components/Hero.js
+++ components/Hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
              <source src="mixkit-cup-full-of-coffee-209.mp4" type="video/mp4"/>
         </video>
       {/* Text container */}
-      <div className="w-full text-center md:text-left lg:p-20 xsm:p-5 sm:p-0">
+      <div className="w-7/12 text-center md:text-left lg:p-20 xsm:p-5 sm:p-0">
         <RoughNotationGroup show={true}>
         <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl xsm:text-2xl font-bold text-gray-700 dark:text-gray-200 my-2">
