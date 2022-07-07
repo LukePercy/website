38edad46b9ca57e3f777395281696fa66eeb7a52
Luke Percy <lpercyagile@gmail.com>
2022-07-07 19:27:08 +1200

    tidy up css

--- components/TheDarkThatDwells.js
+++ components/TheDarkThatDwells.js
@@ -34,7 +34,8 @@ export default function TheDarkThatDwells({...customMeta}){
     title: "The Dark that Dwells Beneath Te Aro",
     description: `First time author L.J. Percy brings a story of a dark threat lurking beneath the Te Aro Valley, a bustling Wellington suburb. Available now on Amazon.com`,
     type: "website",
-    image: userData.avatarUrl,
+    image: '/public/book.png',
+    // image: userData.avatarUrl,
     ...customMeta,
   };
   
@@ -67,10 +68,10 @@ export default function TheDarkThatDwells({...customMeta}){
       <meta property="article:published_time" content={meta.date} />
     )}
   </Head>
-    <main className='w-screen h-screen'>
+    <main className='w-screen'>
       <div className='overflow-hidden'>
         <video autoPlay muted loop id='backgroundVideo' className='w-screen'>
-          <source src="thedarkthatdwellsbackground2.webm" type="video/webm"/>
+          <source src="thedarkthatdwellsbackground.mp4" type="video/mp4"/>
         </video>
         <audio id="track" autoPlay loop>
           <source src="TheDarkThatDwellsSounds.mp3" type="audio/mpeg" />
@@ -81,20 +82,20 @@ export default function TheDarkThatDwells({...customMeta}){
           <div id="play-pause" className="play">Play</div>
         </div>
           <div className='book-page-container'>
-            <div className='book-container'>
+            <div className='Image-area '>
               <img src='book.png' width="600" alt="Book Cover for The Dark That Dwells Beneath Te Aro"/>
             </div>
-              <div className='book-text-container'>
-                <h2 className='reviewText'>
-                <ReactStars count={5} value={4.5} edit={false} size={50} color2={'#ffd700'}/>&#8220; A cleverly written book that does a skilful job of building up a sense of mystery...<br/>&ndash; <span>Amazon Review</span></h2>
-                  <div className='btnContainer'>
+              <div className='Text-area'>
+                <p className='reviewText sm:text-xl md:text-2xl lg:text-4xl'>
+                <ReactStars count={5} value={4.5} edit={false} size={50} color2={'#ffd700'}/>&#8220; A cleverly written book that does a skilful job of building up a sense of mystery...<br/>&ndash; <span>Amazon Review</span></p>
+                  <p className='btnContainer'>
                     <a href='https://www.amazon.com/dp/B09JVFJKFX' target={'_blank'}>
                       <span className='amazonButton'>
                         <i className='iconAmazon'></i>
                           Available Now
                       </span>
                     </a>
-                  </div>
+                  </p>
               </div>
           </div>
         </div>
