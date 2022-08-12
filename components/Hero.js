60d98d7fec3e3b0ff473da66dd6f36818374c3d1
Luke Percy <lpercyagile@gmail.com>
2022-08-13 10:49:03 +1200

    video fixes

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { RoughNotationGroup } from "react-rough-notation";
 import { RainbowHighlight } from "./RainbowHighlight";
 import { Parallax, useParallaxController } from "react-scroll-parallax";
-import bg from '../public/mixkit-cup-full-of-coffee-209_Moment.jpg'
+import bg from '../public/mixkit-cup-full-of-coffee-209_Moment.jpg';
 
 export default function Hero() {
   const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
@@ -20,7 +20,7 @@ export default function Hero() {
     <>
     <div className="relative flex flex-row justify-center items-start overflow-hidden md:pb-20 h-screen">
       <div className="absolute xsm:invisible sm:invisible lg:visible">
-        <Parallax speed={-40} onLoad={handleLoad} easing='easeInOut'>
+        <Parallax speed={-40} onLoad={handleLoad} >
             <video autoPlay muted loop playsInline onCanPlayThrough={videoLoaded} className={`${isVideoLoaded ? "w-screen show" : "w-screen hide"}`}>
             <source src="BlackCoffee.mp4" type="video/mp4"/>
             </video>
@@ -34,7 +34,7 @@ export default function Hero() {
       </div>
       {/* Text container */}
       <div className="w-7/12 text-center md:text-left lg:pt-20 md:pt-20 xsm:pt-10 xsm:pb-10 sm:pt-15">
-      <Parallax speed={10}>
+      <Parallax speed={10} onLoad={handleLoad} easing='easeInOut'>
         <RoughNotationGroup show={true}>
         <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl xsm:text-2xl font-bold text-gray-700 dark:text-gray-200 my-2">
