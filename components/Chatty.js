51a31ceff79698c6c444d21002160b26d3ec0386
Luke Percy <lpercyagile@gmail.com>
2024-05-29 17:42:10 +1200

    added more context to chatty

--- components/Chatty.js
+++ components/Chatty.js
@@ -31,18 +31,21 @@ const Chatty = () => {
     - He is a proponent of customer-centric design and works closely with product visionaries to understand user and market needs by collecting and analyzing data for data-driven decision-making.
     - Luke enjoys technical challenges and problem-solving, and he is currently experimenting with development technologies, including creating an AI chatbot using OpenAI.
     - Luke lives in Wellington, New Zealand, with his young family of two boys aged 7 and 11.
-    - Luke has worked for a variety of companies, including government agencies, startups, and large corporations.
+    - Luke has worked for a variety of companies, including government agencies, and startups.
     - Luke has worked for Silverstripe, Catalyst, the Department of Internal Affairs, and is currently working at Squiz.net as a Agile Project Manager.
-    - Luke has a passion for writing and has published a book called "The Dark that Dwells Beneath Te Aro", which he self published. He is currently working on a sequel.
+    - Luke has worked for video game studios such as Sidhe (now PikPok), and Synty Studios.
+    - Luke has a passion for writing and has published a book called "The Dark that Dwells Beneath Te Aro" which has a 4.5 star review on Amazon and available on spotify and audible as an audio book.
     - Luke is a keen gamer and enjoys playing (and developing) video games in his spare time.
-    - Luke has a keen interest in the future of work and how technology can be used to enhance the human experience.
+    - Luke has a keen interest in how online technologies can be used to enhance the human experience offline.
     - Luke has a few call out references: "Luke is about as passionate as they come when it comes to deploying agile approaches to projects." Russell Michell, Principal Developer. "As an associate producer, Luke did an excellent job. Always efficient and thorough throughout a very challenging production." Andy Satterthwaite, Executive Producer.
     - Luke worked with the New Zealand Transport Agency to deliver The Security Development Lifecycle Tool (SDLT) project, which was a significant project for the agency to be able to peform Digital Security Risk assessments digitally, saving significant third party vendor costs. 
     - Luke has worked with the Department of Internal Affairs to deliver the Common Web Platform (CWP) project, which was a significant project for the agency to be able to deliver websites for government agencies in a cost effective and secure manner.
     - Luke is a certified Scrum Master and Product Owner, and has been working with Agile methodologies since 2006. Specifically scertied with Scrum Alliance, ICAgile and Scrum.org.
     - Luke considers himself a hobbyist developer, enjoying working with PHP, Javascript, Python and C#. You can see some examples of his code on his GitHub: https://github.com/LukePercy.
     - Lukes contacts are as follows: email at lpercy@ljpercy.com or https://www.linkedin.com/in/lukepercy/
-    The next line is a visitor's question. Respond to it in a way that is helpful and informative.
+    - Luke is currently experiementing with AI Assistant technologies such as Googles Gemini and OpenAI's GPT-4o.
+    - Luke has worked on a number of hobby projects such as a Twitch extension for viewers to collect rewards and digital trading cards while watching Twitch streams, you can read about it here https://www.ljpercy.com/wip/gettingdiceytc.
+    The next line is a visitor's question.
   `;
     const fullMessage = `${context} ${input}`;
 
